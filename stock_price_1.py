4ffdc9d6233e5514ea2f4a5df65f239a37dd83d6
neelbando <130071716+neelbando@users.noreply.github.com>
2024-07-30 11:13:45 +0530

    Update stock_price_1.py

--- stock_price_1.py
+++ stock_price_1.py
@@ -1,4 +1,23 @@
 def stock_buy_sell(lst):
+    max_profit = 0
+    
+    l=0
+    r=1
+    while r < len(lst):
+        if lst[l] < lst[r]:
+            profit = lst[r] - lst[l]
+            max_profit = max(max_profit,profit)
+        else:
+            l=r
+            r=r+1
+        r=r+1
+    return max_profit
+    
+lst1 = [1,12,3,5,1,7,4,9,3,11]    #output: 11
+print(stock_buy_sell(lst1))
+
+#---------------------------------------------------------
+def stock_buy_sell(lst):
     cur_profit = 0
     max_profit = 0
     min_buy_price = lst[0]
