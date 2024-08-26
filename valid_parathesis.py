50ef47bfd77e319a69c940cc1b3096f6084d82ef
neelbando <130071716+neelbando@users.noreply.github.com>
2024-08-26 20:09:29 +0530

    Update valid_parathesis.py

--- valid_parathesis.py
+++ valid_parathesis.py
@@ -1,34 +1,3 @@
-# def check_valid_parenthesis(strx):
-#     stk = []
-#     cant_start_with = [")","}","]"]
-
-#     xx = {
-#         "(": ")", 
-#         ")": "(" ,
-#         "{": "}", 
-#         "}": "{" ,
-#         "[": "]", 
-#         "]": "[" 
-#         }
-
-#     for v in strx:
-#         if not stk:
-#             if v in cant_start_with:
-#                 return False
-#             stk.append(v)
-#         else:
-#             if v == xx[stk[-1]]:
-#                 stk.pop()
-#             else:
-#                 if v in cant_start_with:
-#                     return False
-#                 stk.append(v)
-            
-#     if len(stk) > 0:
-#         return False
-  
-#     return True
-
 def check_valid_parenthesis(s: str) -> bool:
     stack = []
     mapping = {")":"(", "}":"{", "]":"["}
